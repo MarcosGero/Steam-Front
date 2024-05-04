a07dd55b668a92028936bdec551b3bbfcfde24aa
Camila Micaela Ramirez <ArtSSimpl@gmail.com>
2024-05-04 13:09:48 -0300

    Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,31 @@ import './Login.css';
 import NavigationBar from '../components/NavigationBar.js'
 import { Form, Button, Container, Row, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom'; 
+import { useState } from 'react';
+import axios from 'axios';
 import Signup from './Signup.js';
+
 function Login() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();  // Evitar el comportamiento de envío por defecto del formulario
+
+    try {
+        const response = await axios.post('http://localhost:8080/users/login', {
+            username: username,
+            password: password
+        });
+
+        console.log('Login successful', response.data);
+        // Manejar la respuesta, por ejemplo, guardar el token de acceso, redirigir al usuario, etc.
+    } catch (error) {
+        console.error('Login failed', error.response || error);
+        // Manejar el error, como mostrar un mensaje al usuario
+    }
+};
   return (
     <div className="App">
       <NavigationBar />
@@ -14,26 +37,33 @@ function Login() {
           <Card className="logincard"> 
             <Card.Body>
               <Form className="form">
+
                 <h3 className="text1 text-left">INICIA SESIÓN CON EL NOMBRE DE LA CUENTA</h3>
+
                 <Form.Group className="mb-3">
-                  <Form.Control className="forms" type="text"/>
+                  <Form.Control className="forms" type="text" value={username}
+                        onChange={(e) => setUsername(e.target.value)} />
                 </Form.Group>
 
                 <h5 className="text-left text2">CONTRASEÑA</h5>
+
                 <Form.Group className="mb-3">
-                  <Form.Control className="forms" type="password"/>
+                  <Form.Control className="forms" type="password" value={password}
+                        onChange={(e) => setPassword(e.target.value)}/>
                 </Form.Group>
 
                 <Form.Group className="text-left text2">
                   <Form.Check type="checkbox" label="Recordarme" />
                 </Form.Group>
 
-                <Button variant="primary" type="submit" className="button">
+                <Button variant="primary" type="submit" className="button" onClick={handleSubmit} >
                   Iniciar sesión
                 </Button>
+
                 <div className="text3 text-muted">
                   <a className="text2" href="#">Ayuda, no puedo iniciar sesión</a>
                 </div>
+
               </Form>
               
             </Card.Body>
