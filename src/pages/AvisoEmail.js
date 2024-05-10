31d36ce84e89de0fa566de667753ba09607b789d
Camila Micaela Ramirez <ArtSSimpl@gmail.com>
2024-05-09 22:16:47 -0300

    Cambio de texto

--- src/pages/AvisoEmail.js
+++ src/pages/AvisoEmail.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './AvisoEmail.css';
 
+
+
 const AvisoEmail = () => {
   return (
     <div style={{ color: 'white', backgroundColor: '#142540', minHeight: '100vh' }} className="email-verification-container">
       <header className='signup-header'>
       <h1>Verifica tu correo electrónico</h1>
       <p>
-        Busca un correo electrónico de Steam en <span className="email-address">marcos.gelves1999@gmail.com</span> para completar la configuración de tu cuenta.
+        Te enviamos un correo electronico para confirmar tu cuenta
       </p>
       <div className="loading-animation">
         {/* Agregar aquí alguna animacion o indicador de carga */}
