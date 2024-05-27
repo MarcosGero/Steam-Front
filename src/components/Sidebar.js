77da3178dbe68c750148eb36b70d50da98d514c1
Camila Micaela Ramirez <ArtSSimpl@gmail.com>
2024-05-27 18:04:49 -0300

    Seccion_noticias #2
    Agregado de estilo

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,7 @@ import './Sidebar.css';
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h2>Centro de Noticias de Steam</h2>
+      <h2>CENTRO DE NOTICIAS DE STEAM</h2>
       <div className="menu-section">
         <h3>Tus Noticias y Eventos</h3>
         <ul>
@@ -19,12 +19,18 @@ function Sidebar() {
           <li>Oficial de Steam</li>
         </ul>
       </div>
+      <h3>BUSCAR</h3>
       <input type="text" placeholder="Buscar" />
       <div className="footer">
-        <p>Noticias de Mentores</p>
-        <button>Opciones y Filtros</button>
-        <button>Solicitar vista adaptativa</button>
-        <p>© 2024 Valve Corporation. Todos los derechos reservados.</p>
+        <h3>DESCUBRIR</h3>
+        <div className="seccion">
+          <button>Noticias de Mentores</button>
+        </div>
+        <div className="seccion">
+          <button>Opciones y Filtros</button>
+          <p>Solicitar vista adaptativa</p>
+        </div>
+        <p>© 2024 Valve Corporation. Todos los derechos reservados.Todas las marcas registradas pertenecen a sus respectivos dueños</p>
       </div>
     </div>
   );
