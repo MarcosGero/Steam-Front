0a9942650b3489014ae5d1e5323476a903307b04
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2024-06-06 18:46:09 -0300

    #1 Seccion Cuenta Detalle
    Estilizacion al encabezado

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,8 @@ const Header = () => { // Esto aparece como encabezado siempre en la pagina stea
     <Box sx={{
       bgcolor: "#142540",
       p: 2,
+      paddingBottom : '30px',
+      paddingTop : '30px',
       color: 'white',
       backgroundImage: 'url(/cluster_bg_40.png)',
       backgroundRepeat: 'no-repeat',
@@ -13,16 +15,16 @@ const Header = () => { // Esto aparece como encabezado siempre en la pagina stea
       backgroundSize: 'cover'  // Esto asegura que la imagen cubra todo el espacio disponible.
     }}>
       <Breadcrumbs aria-label="breadcrumb">
-        <Link underline="hover" color="white" href="https://store.steampowered.com/">
+        <Link underline="hover" color="#808891" href="https://store.steampowered.com/">
           Inicio
         </Link>
-        <Typography color="white">Cuenta</Typography>
+        <Typography sx = {{color:'#808891',fontFamily: 'Motiva Sans, Arial, Helvetica, sans-serif'}} >Cuenta</Typography>
       </Breadcrumbs>
-      <Typography variant="h2" sx={{ mt: 2 }}>
+      <Typography variant="h2" sx={{ mt: 2}}>
         Cuenta de {localStorage.getItem("local-user")}
       </Typography>
-      <Typography variant="body1">
-        Id. de Steam: 
+      <Typography variant="body1" sx = {{color:'#808891', marginTop : '3px'}}>
+        Id. de Steam: 76561199023222600
       </Typography>
     </Box>
   );
