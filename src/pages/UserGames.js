c855cc0143651e43cb11f502266f1af673da1e7a
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2024-06-12 20:47:20 -0300

    #Seccion juegos comprados #7
    Logica de acceso a bd implementada a medias

--- src/pages/UserGames.js
+++ src/pages/UserGames.js
@@ -1,12 +1,60 @@
 import './UserGames.css';
 import GameItem from "../components/GameItem";
+
+// -- Esto es de prueba, luego sacar --
 import img_1 from "../resources/ejemplo.jpg"
 import img_2 from "../resources/ejemplo_2.jpg"
 import img_3 from "../resources/ejemplo_3.jpg"
+// ------------------------------------
 
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import axios from "axios";
+import {API_URL} from "../config/env";
 
+// {user.name}
+
+/*
+    {games.map(game => (
+        <GameItem
+           key={games._id}
+           img={`http://localhost:8080/api/v1/games/images/${games.thumbnail}`}
+           title={games.name}
+           time={games.playTime}
+           date={games.purchaseDate}
+         />
+     ))}
+*/
 
 function UserGames () {
+
+    const [games, setGames] = useState([]);
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await axios.get(API_URL + 'user/me');
+                setUser(response.data);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
+        fetchUserData();
+    }, []);
+
+    useEffect(() => {
+
+        Axios.get(API_URL + 'users/'+user._getId+'/games')
+            .then(response => {
+                setGames(response.data);
+            })
+            .catch(error => {
+                console.error('Hubo un error al recuperar los juegos del usuario!', error);
+            });
+    }, []);
+
     return(
         <div className="user-back-game">
          <div className="user-header-game">
@@ -46,9 +94,24 @@ function UserGames () {
                  <a> ESTADO DE DESCARGA REMOTA: Sin acceso a tu PC</a>
              </div>
              <div className='contenedor-game'>
-                 <GameItem img={img_1} title='NaissanceE' time='114 minutos' date='11 de oct de 2021'/>
-                 <GameItem img={img_2} title='Despotism 3K' time='113 minutos' date='11 de ago de 2022'/>
-                 <GameItem img={img_3} title='BitBurner' time='70 minutos' date='21 de dic de 2021'/>
+                 <GameItem
+                     img={img_1}
+                     title='NaissanceE'
+                     time='114 minutos'
+                     date='11 de oct de 2021'
+                 />
+                 <GameItem
+                     img={img_2}
+                     title='Despotism 3K'
+                     time='113 minutos'
+                     date='11 de ago de 2022'
+                 />
+                 <GameItem
+                     img={img_3}
+                     title='BitBurner'
+                     time='70 minutos'
+                     date='21 de dic de 2021'
+                 />
              </div>
          </div>
         </div>
