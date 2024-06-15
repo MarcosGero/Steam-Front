4aa3e99e34351a2bebe43b8bb99e1c3dcc884194
Camila Micaela Ramirez <75436179+Cami3999@users.noreply.github.com>
2024-06-14 22:44:43 -0300

    update

--- src/pages/ConfirmPurchase.js
+++ src/pages/ConfirmPurchase.js
@@ -7,7 +7,6 @@ import axios from "axios";
 function ConfirmPurchase() {
     const [cartGames, setCartGames] = useState([]);
     const [totalPrice, setTotalPrice] = useState(0);
-    const [cartera, setCartera] = useState(0);
     const [userName, setUserName] = useState('');
     const [termsAccepted, setTermsAccepted] = useState(false);
     const navigate = useNavigate();
